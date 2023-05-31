9bb3458880a912e250ffb6a543ab7b736cdcc402
irikosouma <thanh.iri112@gmail.com>
2023-05-31 21:01:10 +0700

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME HOME
+          WELCOME HOME ok
         </p>
         <a
           className="App-link"
